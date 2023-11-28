4666cc9fd0ca7f66ee3366ba611b5d3db8671e8b
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-11-28 18:06:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 function firstChar(text) {
-  // your code here
+	// your code here
+	text = text.trim();
+
+	if (text === '') {
+		return '';
+	}
+
+	for (let index = 0; index < text.length; index++) {
+		if (text[i] !== ' ') {
+			return text[i];
+		}
+	}
+
+	return '';
 }
+
+console.log(firstChar(' Rosa Parks '));    
+console.log(firstChar('  Hello World '));   
+console.log(firstChar('   '));              
+console.log(firstChar(''));
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
+const text = prompt("Enter text:"); 
 alert(firstChar(text));
